501f97a21b6263220449135de5bbe103d6b1abf5
Joe C <38189554+Praxiliphicuz@users.noreply.github.com>
2023-08-14 12:46:03 -0400

    PY4E 4
    My fourht

---
+++ PY4E 4/3RDDataBase_.py
@@ -0,0 +1,40 @@
+import sqlite3
+# import sqlite3 library
+
+conn = sqlite3.connect('emaildb.sqlite')
+# then we creat a connection
+
+cur = conn.cursor()
+# So in making that connection to the databases, we sort of end up with an open thats two steps. That theres the connnection to the database which checks access to the file and the cursor is kind of like our handle. Its NOT as simple as just open and reading it, but here you open and then you send SQL commands the cursor and then you get your response thropugh that same cursor.
+# the file email.sqlite doesnt exist before all this, so we make it exist by making the variable we did
+cur.execute('DROP TABLE IF EXISTS Counts')
+#no table exists so this is useless...for now
+
+cur.execute('''
+CREATE TABLE Counts (org email TEXT, count INTEGER)''')
+#this test is only has triple quotes to show its longer
+
+fname = input("Enter file name: ")
+if (len(fname) < 1): fname = 'mbox-short.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith("From: "): continue
+    pieces = line.split()
+    email = pieces[1]
+    cur.execute('SELECT count FROM Counts WHERE org email = ? ', (email,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (email,count)
+                VALUES (?, 1)''',(email,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
+                    (email,))
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
---
+++ PY4E 4/OrientedObjectsEx1ch14_.py
@@ -0,0 +1,26 @@
+#So first off we have a new key word called class and class sort of acts like a function
+#Then we provide a name of the class & then an indented block to continue the code
+#so in the class here we just have x = 0, thats an attribute of all party animals will have a variable named x in them and theres a lttle functon. So this is now a method
+class PartyAnimal:
+    x = (0)
+
+#this code is a part of the block of the code above, every class has some data associated with that class and some code associated with that class
+    def party(self) :
+        self.x = self.x + 1
+        print("So far " ,self.x)
+
+#The method above has a bit of code in it, but when this is done, it doesnt actually run any code, but what happens is we have a template called PartyAnimal.
+# And The syntax here below    an = PartyAnimal()    is basically saying make me a PartyAnimal, its the same as saying x = list(), which says mint me a new list, there is a template for a new list here and then give that empty list back in the variable of x
+# once thats empty or fresh PartyAnimal is done, then give it back to me in the variable an, and so this then is an OBJECT
+an = PartyAnimal()
+
+# So here we make a call by taking name of the object .  dot it the operator and then the method within it, its calls it 3 times
+an.party()
+an.party()
+an.party()
+
+
+
+#we can use more parameters in this class we made/ called, the second party animal has no parameters while the first has one parameters, which is (self)
+#And what you basically can think of this as, is its kind've like saying, go into party animal, call the party function within that and then pass, an , as the first parameter variabel, which comes in as (self), most people name the variable salf.
+# And what self says   self.x = self.x + 1     well thats kind've like saying      an..x = an.x + 1       because were calling it in the context of an
